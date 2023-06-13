e728f2a3c8fe6a28e8e7b065ea180975b160cc77
Dmytro <srk90512@gmail.com>
2023-06-13 03:03:48 +0300

    Need to add id for buttons and listen them

--- js_calc/src/components/Buttons.js
+++ js_calc/src/components/Buttons.js
@@ -4,39 +4,59 @@ export class Buttons extends Component {
   render() {
     return (
       <div>
-        <button 
+        <button
+            className='jumbo'
+            onClick={this.props.initialize}
             value='AC'>AC</button>
         <button
+            onClick={this.props.operators}
             value='/'>/</button>
         <button
+            onClick={this.props.operators}
             value='x'>x</button>
         <button
+            onClick={this.props.numbers}
             value='7'>7</button>
         <button
+            onClick={this.props.numbers}
             value='8'>8</button>
         <button
+            onClick={this.props.numbers}
             value='9'>9</button>
         <button
+            onClick={this.props.operators}
             value='-'>-</button>
         <button
+            onClick={this.props.numbers}
             value='4'>4</button>
         <button
+            onClick={this.props.numbers}
             value='5'>5</button>
         <button
+            onClick={this.props.numbers}
             value='6'>6</button>
         <button
+            onClick={this.props.operators}
             value='+'>+</button>
         <button
+            onClick={this.props.numbers}
             value='1'>1</button>
         <button
+            onClick={this.props.numbers}
             value='2'>2</button>
         <button
+            onClick={this.props.numbers}
             value='3'>3</button>
         <button
+            className='jumbo'
+            onClick={this.props.numbers}
             value='0'>0</button>
         <button
+            onClick={this.props.decimal}
             value='.'>.</button>
         <button
+            className='equals'
+            onClick={this.props.evaluate}
             value='='>=</button>
       </div>
     )
